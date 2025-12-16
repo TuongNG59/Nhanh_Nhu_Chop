e6cf2ca6a76a172b5af7beaae888a52059439902
TuongNG59 <nhuynhtuong59@gmail.com>
2025-12-16 18:41:51 +0700

    Create MainActivity, PlayActivity, QuestionListActivity, ScoreActivity. Hoàn thành việc chuyển màn hình.

--- Project_Android/app/src/main/java/clc65/tuongng59/nguyenhuynhtuong/project_android/MainActivity.java
+++ Project_Android/app/src/main/java/clc65/tuongng59/nguyenhuynhtuong/project_android/MainActivity.java
@@ -1,6 +1,8 @@
 package clc65.tuongng59.nguyenhuynhtuong.project_android;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +12,34 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnPlay, btnManage, btnScore;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        btnPlay = findViewById(R.id.btnPlay);
+        btnManage = findViewById(R.id.btnManage);
+        btnScore = findViewById(R.id.btnScore);
+
+        //  Chuyển qua màn hình chơi
+        btnPlay.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, PlayActivity.class);
+            startActivity(intent);
+        });
+
+
+       //   Chuyển qua màn hình quản lý câu hỏi
+        btnManage.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, QuestionListActivity.class);
+            startActivity(intent);
+        });
+
+        //  Chuyển qua màn hình điểm
+        btnScore.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, ScoreActivity.class);
+            startActivity(intent);
         });
     }
 }
---
+++ Project_Android/app/src/main/java/clc65/tuongng59/nguyenhuynhtuong/project_android/PlayActivity.java
@@ -0,0 +1,19 @@
+package clc65.tuongng59.nguyenhuynhtuong.project_android;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class PlayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play);
+
+    }
+}
